76a3973e9a2400d91da8da62fa42edc4538a8ce0
Bruno Morais <bruno@MBP13.local>
2023-10-19 11:03:57 -0300

    atualizacao do datalayer

--- example/config.php
+++ example/config.php
@@ -4,9 +4,9 @@ const CONFIG_DATA_LAYER = [
     "driver" => "mysql",
     "host" => "localhost",
     "port" => "3306",
-    "dbname" => "dbname",
-    "username" => "user_database",
-    "passwd" => "password_database",
+    "dbname" => "",
+    "username" => "",
+    "passwd" => "",
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8, lc_time_names = 'pt_BR'",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
